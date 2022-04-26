4837e52daecbb87328636cd46b5a0e91bdec317c
zee6197 <zeeman@berkeley.edu>
2022-04-26 13:08:25 -0700

    First commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Subject = "CSharp";
+            Console.WriteLine("Please choose a subject: ");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "CSharp":
+                    Console.WriteLine("You are in one of the best classes!");
+                    break;
+
+                case "Chemisty":
+                    Console.WriteLine("Wow that's a first!");
+                    break;
+
+                case "Computer Science":
+                    Console.WriteLine("You rock!");
+                    break;
+
+                case "Physics":
+                    Console.WriteLine("You are doing great!");
+                    break;
+
+                case "English":
+                    Console.WriteLine("Way to go Shakespheare!");
+                    break;
+                default:
+                    Console.WriteLine("Sorry, but this is not a valid input!");
+                    break;
+            }
+
+        }
+    }
+}
